1d207edd3ca0db3ce92a4426d541e0dbdbb38001
Cécile Guillot <cecile.guillot@live.fr>
2022-08-04 22:06:30 +0000

    Upload New File

---
+++ 02-MapReduce/sum_amount.py
@@ -0,0 +1,21 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class Sum_amount(MRJob):  
+    def steps(self):
+        return [
+            MRStep(mapper = self.mapper_amount,
+                   reducer = self.reducer_amount)
+        ]
+    
+    def mapper_amount(self, _, line):
+        agency_id,account_sender_name,country_sender,account_receiver_name,country_receiver,amount,payment_type,datetime_timestamp = line.split(',')
+        if amount != 'amount':
+            if account_sender_name == account_receiver_name:
+                yield (account_receiver_name, int(amount))
+    
+    def reducer_amount(self, key, values):
+        yield key, sum(values)
+
+if __name__ == "__main__":
+    Sum_amount.run()
