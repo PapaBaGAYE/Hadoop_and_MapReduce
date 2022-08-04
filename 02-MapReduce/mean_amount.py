bfca0556b30da7a27561591df2247a537c0e0f35
Cécile Guillot <cecile.guillot@live.fr>
2022-08-04 22:05:57 +0000

    Upload New File

---
+++ 02-MapReduce/mean_amount.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import statistics
+
+class Mean_amount(MRJob):  
+    def steps(self):
+        return [
+            MRStep(mapper = self.mapper_mean,
+                   reducer = self.reducer_mean)
+        ]
+    
+    def mapper_mean(self, _, line):
+        agency_id,account_sender_name,country_sender,account_receiver_name,country_receiver,amount,payment_type,datetime_timestamp = line.split(',')
+        if amount != 'amount':
+            if account_sender_name == account_receiver_name:
+                yield (account_receiver_name, int(amount))
+    
+    def reducer_mean(self, key, values):
+        yield key, statistics.mean(values)
+
+if __name__ == "__main__":
+    Mean_amount.run()
