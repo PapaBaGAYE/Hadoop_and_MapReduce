9fbf2131cb1e76b80e702e31fa165569c717868b
Cécile Guillot <cecile.guillot@live.fr>
2022-08-06 20:16:23 +0000

    Adding doctstring

--- 02-MapReduce/customer_agency.py
+++ 02-MapReduce/customer_agency.py
@@ -1,8 +1,10 @@
+"""The aim of this script is to count the number of customer by agency.
+   To realize that, MapReduce was used in order to work on a Hadoop cluster."""
+
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import statistics
 
-class Count_customer_agency(MRJob):  
+class Count_customer_agency(MRJob):
     def steps(self):
         return [
             MRStep(mapper = self.mapper_count_customer,
@@ -10,7 +12,9 @@ class Count_customer_agency(MRJob):
         ]
     
     def mapper_count_customer(self, _, line):
-        agency_id,account_sender_name,country_sender,account_receiver_name,country_receiver,amount,payment_type,datetime_timestamp = line.split(',')
+        agency_id,account_sender_name,country_sender,\
+        account_receiver_name,country_receiver,amount,\
+        payment_type,datetime_timestamp = line.split(',')
         yield (agency_id, 1)
     
     def reducer_count_customer(self, key, values):
